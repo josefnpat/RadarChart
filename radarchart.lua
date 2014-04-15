452b59ec9f5dfbfd874850f8fba0453f7e067676
josefnpat <josef.patoprsty@redbottledesign.com>
2014-04-15 15:15:20 -0400

    Adding meta fields ( http://kiki.to/blog/2014/03/31/rule-2-return-a-local-table/ )

--- radarchart.lua
+++ radarchart.lua
@@ -4,7 +4,22 @@
 -- @copyright 2014
 -- @license <a href="http://www.opensource.org/licenses/zlib-license.php">zlib/libpng</a>
 
-local RadarChart = {}
+local RadarChart = {
+  _VERSION = "RadarChart v1.0.0",
+  _DESCRIPTION = "LÖVE module fir drawing radar/spider charts.",
+  _URL = "https://github.com/josefnpat/RadarChart/",
+  _LICENSE = [[
+    The zlib/libpng License
+
+    Copyright (c) 2014 Josef N Patoprsty
+
+    This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+    Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+    1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+    2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+    3. This notice may not be removed or altered from any source distribution.
+  ]]
+}
 
 --- Instansiate a new instance of a radar chart.
 -- @param data <i>Required</i> The data set that the radar chart should operate on. See <a href="#RadarChart:setData">RadarChart.setData</a> for information on the structure of this object.
